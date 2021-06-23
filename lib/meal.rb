f60b2cca24b82ee25329f4702a410abab7a27dd3
Pranik Chainani <pranikc@gmail.com>
2021-06-23 15:16:30 -0400

    commit tree a28e3c81724887866075b9e29ecfa547ce80f24b

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,15 @@
 class Meal
+    attr_accessor :waiter, :customer, :tip
+    @@all = []
 
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
